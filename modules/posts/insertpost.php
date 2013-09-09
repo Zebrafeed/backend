e474c805a0c3d7efcdfe9fd3d8963994de5ade74
John Eriksson <yanis.cool3@gmail.com>
2013-09-09 22:21:07 +0200

    created an insert file for inserting posts into db

---
+++ modules/posts/insertpost.php
@@ -0,0 +1,21 @@
+<?php 
+	require "post.php";
+	//require "connect.php";
+
+	$title = mysql_real_escape_string($_POST['title']);
+	$text = mysql_real_escape_string(ln2br($_POST['text']));
+	$created = date("l jS \of F Y h:i:s A");
+	$author = mysql_real_escape_string($_POST['author']);
+
+	if(isset($title, $text, $created, $author)){
+		$post = new Post("", $title, $text, $created, $author);
+
+		$sql = mysql_query("INSERT INTO posts ('title', 'text', 'created', 'author') VALUES ($title, $text, $created, $author)");
+
+		if($sql){
+			echo "Post successful";
+		}else{
+			echo "Failed to post";
+		}
+	}
+?>
