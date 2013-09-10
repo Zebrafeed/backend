da5fa886a0cc8050cad1f0626ac47dbbec8dd822
John Eriksson <yanis.cool3@gmail.com>
2013-09-10 17:50:26 +0200

    Minor fix with category, added default value 'uncategorized'

--- modules/posts/insertpost.php
+++ modules/posts/insertpost.php
@@ -5,8 +5,13 @@
 	$text = mysql_real_escape_string(ln2br($_POST['text']));
 	$created = date("l jS \of F Y h:i:s A");
 	$author = mysql_real_escape_string($_POST['author']);
-	$cat = mysql_real_escape_string($_POST['cat'])
-
+	
+	if(isset($_POST['cat'])){
+		$cat = mysql_real_escape_string($_POST['cat']);
+	}else{
+		$cat = "uncategorized";
+	}
+	
 	if(isset($title, $text, $created, $author)){
 		$sql = mysql_query("INSERT INTO posts ('title', 'text', 'created', 'author', 'cat') VALUES ($title, $text, $created, $author, $cat)");
 
