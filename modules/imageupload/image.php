e8f8c09edc58826d382aee808a04a5027c8a8d5c
John Eriksson <yanis.cool3@gmail.com>
2013-09-10 03:54:07 +0200

    created imageupload module with image.php class

---
+++ modules/imageupload/image.php
@@ -0,0 +1,86 @@
+<?php
+	/**
+	* Image class
+	*/
+	class Image{
+
+		public $name = "";
+		public $desc = "";
+		public $uri = "";
+		
+		function __construct($name, $desc, $uri){
+			$this->name = $name;
+			$this->desc = $desc;
+			$this->uri = $uri;
+		}
+
+		/**
+		 * Getter for name
+		 *
+		 * @return mixed
+		 */
+		public function getName()
+		{
+		    return $this->name;
+		}
+		
+		/**
+		 * Setter for name
+		 *
+		 * @param mixed $name Value to set
+		 * @return self
+		 */
+		public function setName($name)
+		{
+		    $this->name = $name;
+		    return $this;
+		}
+
+		/**
+		 * Getter for desc
+		 *
+		 * @return mixed
+		 */
+		public function getDesc()
+		{
+		    return $this->desc;
+		}
+		
+		/**
+		 * Setter for desc
+		 *
+		 * @param mixed $desc Value to set
+		 * @return self
+		 */
+		public function setDesc($desc)
+		{
+		    $this->desc = $desc;
+		    return $this;
+		}
+
+		/**
+		 * Getter for uri
+		 *
+		 * @return mixed
+		 */
+		public function getUri()
+		{
+		    return $this->uri;
+		}
+		
+		/**
+		 * Setter for uri
+		 *
+		 * @param mixed $uri Value to set
+		 * @return self
+		 */
+		public function setUri($uri)
+		{
+		    $this->uri = $uri;
+		    return $this;
+		}
+		
+		
+		
+	}
+?>
