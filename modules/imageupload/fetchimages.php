90c9f0712685e219392d97e398d7b49f19a55cf9
John Eriksson <yanis.cool3@gmail.com>
2013-09-10 06:08:39 +0200

    added ability to fetch json for images

---
+++ modules/imageupload/fetchimages.php
@@ -0,0 +1,13 @@
+<?php
+	require 'image.php';
+	require '../../connect.php';
+
+	$images = array();
+	$sql = mysql_query("SELECT * FROM images");
+
+	while ($row = mysql_fetch_object($sql)) {
+		$images[] = new Image($row->name, $row->desc, $row->uri);
+	}
+
+	echo json_encode($images);
+?>
